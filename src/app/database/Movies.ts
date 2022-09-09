0c9057ac38578f58e10de7810c921c8c456c4679
caducasara <carloscasara98@gmail.com>
2022-09-09 10:44:10 -0300

    REFACTOR: Add static movie folders

--- src/app/database/Movies.ts
+++ src/app/database/Movies.ts
@@ -2,7 +2,7 @@ export const getMovies = () => ([
   {
     id: 1,
     title: 'The House of the Dragon ',
-    folder: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ruHPkmmAwBTCzNXtHot18rs6ctN.jpg',
+    folder: 'assets/folders/house_of_dragon.jpg',
     synopsi: '200 years before the events of "Game of Thrones", the Targaryens were at the height of their power, having countless dragons at their command, but not everything lasts forever. The beginning of the end of the Targaryen dynasty.',
     year: '2022',
     category: 'Fantasy',
@@ -19,7 +19,7 @@ export const getMovies = () => ([
   {
     id: 2,
     title: 'Top Gun: Maverick',
-    folder: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/jTrXwK56EoLHHxfBkpwGdfmy2uh.jpg',
+    folder: 'assets/folders/top_gun.jpg',
     synopsi: "After more than 30 years of service as one of the Navy's top aviators, Pete 'Maverick' Mitchell is back, pushing the limits as a courageous test pilot. In the contemporary world of tech wars, Maverick takes on drones and proves that the human factor is still essential.",
     year: '2022',
     category: 'Action',
@@ -36,7 +36,7 @@ export const getMovies = () => ([
   {
     id: 3,
     title: 'She-Hulk: Defender of Heroes',
-    folder: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/4xNH5hHPpTJptSWANyAX3OLZoNM.jpg',
+    folder: 'assets/folders/she_hulk.jpg',
     synopsi: "Jennifer Walters — a lawyer specializing in cases involving superhumans — must deal with the complicated life of a single woman in her early thirties who happens to be a seven-foot super-powered hulk.",
     year: '2022',
     category: 'Comedy',
@@ -53,7 +53,7 @@ export const getMovies = () => ([
   {
     id: 4,
     title: 'DC League of Superpets',
-    folder: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/oYIbivi986h4M8ZNQiwKSzIF8KJ.jpg',
+    folder: 'assets/folders/super_pets.jpg',
     synopsi: "DC Super Pets follows Krypto the Superdog and Superman, inseparable friends, sharing the same superpowers and fighting crime in Metropolis side by side. When Superman and the rest of the Justice League are kidnapped, Krypto must convince a pack of shelters - Ace the dog, PB the pot-bellied pig, Merton the turtle and Chip the squirrel - to harness his own newfound powers and help him to Rescue the Superheroes.",
     year: '2022',
     category: 'Animation',
@@ -70,7 +70,7 @@ export const getMovies = () => ([
   {
     id: 5,
     title: 'Sandman',
-    folder: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/q54qEgagGOYCq5D1903eBVMNkbo.jpg',
+    folder: 'assets/folders/sandman.jpg',
     synopsi: "A wizard tries to capture Death to bargain for eternal life, however, ends up trapping his younger brother, Dream. Fearing for his safety, the mage keeps him trapped in a glass bottle for decades. After his escape, Dream, also known as Morpheus, sets out in search of his powerful lost objects.",
     year: '2022',
     category: 'Drama',
@@ -87,7 +87,7 @@ export const getMovies = () => ([
   {
     id: 6,
     title: 'Breaking Bad',
-    folder: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/30erzlzIOtOK3k3T3BAl1GiVMP1.jpg',
+    folder: 'assets/folders/breaking_bad.jpg',
     synopsi: "Upon learning that he has cancer, a professor starts manufacturing methamphetamine for the family's future, changing everyone's destiny.",
     year: '2008',
     category: 'Drama',
@@ -104,7 +104,7 @@ export const getMovies = () => ([
   {
     id: 7,
     title: 'Red: Growing Up is a Beast',
-    folder: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/725WE0Qb1BbUF7aGvjiQqzzffpg.jpg',
+    folder: 'assets/folders/red.jpg',
     synopsi: "Thirteen-year-old Mei is experiencing the awkwardness of being a teenager with a twist – when she gets too excited, she transforms into a giant red panda.",
     year: '2022',
     category: 'Animation',
@@ -121,7 +121,7 @@ export const getMovies = () => ([
   {
     id: 8,
     title: 'The Boys',
-    folder: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/stTEycfG9928HYGEISBFaG1ngjM.jpg',
+    folder: 'assets/folders/the_boys.jpg',
     synopsi: "In the plot, we know a world where superheroes are the biggest celebrities on the planet, and routinely abuse their powers instead of using them for good.",
     year: '2019',
     category: 'Action',
@@ -138,7 +138,7 @@ export const getMovies = () => ([
   {
     id: 9,
     title: "Paying well, what's wrong with that?",
-    folder: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/2gL5KB4GV51O6rH9HTeDGeTy0XX.jpg',
+    folder: 'assets/folders/pagando_bem.jpg',
     synopsi: "Zack Brown (Seth Rogen) and Miriam Linky (Elizabeth Banks) have been friends for a long time, and they currently share a house and have several debts. After having the water and electricity cut off, they decide to make a home porn movie to get some money. In this way they select some friends to help them, swearing that sex will not harm the existing friendship. But when the recordings start, the business becomes something much bigger than they imagined.",
     year: '2008',
     category: 'Comedy',
@@ -155,7 +155,7 @@ export const getMovies = () => ([
   {
     id: 10,
     title: "Supernatural",
-    folder: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ovFzkkKknAo2SbM2DfOabxRvzmy.jpg',
+    folder: 'assets/folders/supernatural.jpg',
     synopsi: "Brothers Dean and Sam scour the country for paranormal activity, fighting demons, ghosts and monsters along the way.",
     year: '2005',
     category: 'Fantasy',
@@ -172,7 +172,7 @@ export const getMovies = () => ([
   {
     id: 11,
     title: "Peaky Blinders: Blood, Bets and Razors",
-    folder: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/i0uajcHH9yogXMfDHpOXexIukG9.jpg',
+    folder: 'assets/folders/peaky_blinders.jpg',
     synopsi: "Thomas Shelby and his brothers return to Birmingham after serving in the British Army during World War I. The Peaky Blinders, the gang in which Thomas is a leader, control the city of Birmingham. But as Shelby's ambitions extend beyond Birmingham, he plans to build the business empire he's created and stop anyone who gets in the way.",
     year: '2013',
     category: 'Drama',
@@ -189,7 +189,7 @@ export const getMovies = () => ([
   {
     id: 12,
     title: "Vikings",
-    folder: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/bQLrHIRNEkE3PdIWQrZHynQZazu.jpg',
+    folder: 'assets/folders/vikings.jpg',
     synopsi: "Ragnar Lothbrok, a young man who considers himself a descendant of Odin, is a warrior, farmer and householder. Visionary and dreaming of living different adventures, he believes that the riches dreamed of by his people lie beyond the Baltic Sea. But in order to conquer them, he must convince Earl Haraldson, the leader of his people, to allow him and his warriors to travel to England.",
     year: '2013',
     category: 'Drama',
@@ -206,7 +206,7 @@ export const getMovies = () => ([
   {
     id: 13,
     title: "Tomorrow's Express",
-    folder: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/mt7mmif11QXf8hsfgDAlOGJwJcV.jpg',
+    folder: 'assets/folders/expresso_amanha.jpg',
     synopsi: "A group of researchers creates an experiment capable of putting an end to the threat of global warming, but their plans don't go as planned and they end up wiping out almost all life on Earth, which, in turn, turns into an ice desert. . The only survivors roam the planet aboard a train called Snowpiercer, but not everyone is resigned to the situation. With that, a revolution is about to break out.",
     year: '2020',
     category: 'Drama',
@@ -223,7 +223,7 @@ export const getMovies = () => ([
   {
     id: 14,
     title: "Locke & Key",
-    folder: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/zuxGfRKziGHPogipnEXXykdDmyT.jpg',
+    folder: 'assets/folders/lock_key.jpg',
     synopsi: "After their father's murder, three brothers move with their mother to an old house where they discover magical keys that reveal powers and secrets.",
     year: '2020',
     category: 'Fantasy',
@@ -240,7 +240,7 @@ export const getMovies = () => ([
   {
     id: 15,
     title: "The Witcher",
-    folder: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/e5iqR3k7azXGK1tGaOLMnKycGku.jpg',
+    folder: 'assets/folders/the_witcher.jpg',
     synopsi: "The mutant Geralt of Rivia is a monster hunter who struggles to find his place in a world where people are often more evil than wild creatures.",
     year: '2019',
     category: 'Fantasy',
@@ -257,7 +257,7 @@ export const getMovies = () => ([
   {
     id: 16,
     title: "Lucifer",
-    folder: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/hdKxcoV5CFc3sGOmbGXDXbx1cTZ.jpg',
+    folder: 'assets/folders/lucifer.jpg',
     synopsi: "Bored with life in the dark, the devil moves to Los Angeles, opens a piano bar and lends his wisdom to a murder investigator.",
     year: '2016',
     category: 'Fantasy',
@@ -274,7 +274,7 @@ export const getMovies = () => ([
   {
     id: 17,
     title: "The Ranch",
-    folder: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/lrbHBAxF9Ir83rxgeBrfmEJriLt.jpg',
+    folder: 'assets/folders/the_ranch.jpg',
     synopsi: "Set on a Colorado ranch, The Ranch stars Ashton Kutcher as Colt, a semi-professional football player who returns from the dugout to help run the family farm with his older brother Jameson “Rooster” (Danny Masterson) and his father. Beau (Sam Elliott), whom he hasn't seen in 15 years. Debra Winger is Maggie, the mother of Colt and Rooster and the manager of the nearest town bar.",
     year: '2016',
     category: 'Comedy',
@@ -291,7 +291,7 @@ export const getMovies = () => ([
   {
     id: 18,
     title: "Brooklyn Nine-Nine",
-    folder: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/hgRMSOt7a1b8qyQR68vUixJPang.jpg',
+    folder: 'assets/folders/brooklyn_nine-nine.jpg',
     synopsi: "Brilliant, immature detective Jake Peralta must learn to play by the rules and work as a team when a demanding captain takes command of his squad.",
     year: '2013',
     category: 'Comedy',
@@ -308,7 +308,7 @@ export const getMovies = () => ([
   {
     id: 19,
     title: "How I Met Your Mother",
-    folder: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/578tlvbrpFwc959bQTT93W2RYA.jpg',
+    folder: 'assets/folders/himym.jpg',
     synopsi: "In the year 2030, Ted Mosby, an architect, decides to explain to his children the story of how he met their mother. It all started in 2005, when one of his friends decided to get married and Ted needed to find love.",
     year: '2005',
     category: 'Comedy',
@@ -325,7 +325,7 @@ export const getMovies = () => ([
   {
     id: 20,
     title: "John Wick: Back in the Game",
-    folder: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/rboZslo3eQWKBQ3QvlO6wGV0J3K.jpg',
+    folder: 'assets/folders/john-wick.jpg',
     synopsi: "John Wick is a legendary retired hitman dealing with grief after losing the love of his life. When a gangster breaks into his house, kills his dog and steals his car, he is forced back into Action and begins his revenge.",
     year: '2014',
     category: 'Action',
@@ -342,7 +342,7 @@ export const getMovies = () => ([
   {
     id: 21,
     title: "Rescue",
-    folder: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/5DS1Xh1dxfrR1H0sqATPxkwWFzi.jpg',
+    folder: 'assets/folders/rescue.jpg',
     synopsi: "In Bangladesh, mercenary Tyler Rake struggles to survive during his mission to rescue the son of a crime boss.",
     year: '2020',
     category: 'Action',
@@ -359,7 +359,7 @@ export const getMovies = () => ([
   {
     id: 22,
     title: "Shrek",
-    folder: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/iB64vpL3dIObOtMZgX3RqdVdQDc.jpg',
+    folder: 'assets/folders/shreck.jpg',
     synopsi: "In a distant swamp lives Shrek (Mike Myers), a lonely ogre who sees his life being invaded by a series of fairy tale characters, such as three blind mice, a big and evil wolf and even three pigs. who don't have a place to live. All of them were driven from their homes by the evil Lord Farquaad (John Lithgow). Determined to regain his former tranquility, Shrek resolves to find Farquaad and makes a deal with him: all the characters will be able to return to their homes if he and his friend Donkey (Eddie Murphy) rescue a beautiful princess (Cameron Diaz), who is a prisoner of a dragon. However, when Shrek and Donkey finally manage to rescue the princess, they soon discover that their troubles are just beginning.",
     year: '2001',
     category: 'Animation',
@@ -376,7 +376,7 @@ export const getMovies = () => ([
   {
     id: 23,
     title: "The Beast of the Sea",
-    folder: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/5KBOy36wOAkAorpvHwvzEwQdJeR.jpg',
+    folder: 'assets/folders/beast_of_sea.jpg',
     synopsi: "A sea monster hunter's life is turned upside down after a little girl hides on his ship.",
     year: '2022',
     category: 'Animation',
@@ -393,7 +393,7 @@ export const getMovies = () => ([
   {
     id: 24,
     title: "Moana: A Sea of ​​Adventure",
-    folder: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/4jNbH7A9HGL4OCNQvzj98ksoa2f.jpg',
+    folder: 'assets/folders/moana.jpg',
     synopsi: "Moana Waialiki is a courageous young woman, daughter of the chief of a tribe in Oceania, coming from a long line of navigators. Wanting to find out more about her past and help her family, she decides to go in search of her ancestors, inhabitants of a mythical island that no one knows where it is. Accompanied by the legendary demigod Maui, Moana begins her journey into the open sea, where she faces terrifying sea creatures and discovers stories of the underworld.",
     year: '2016',
     category: 'Animation',
@@ -410,7 +410,7 @@ export const getMovies = () => ([
   {
     id: 25,
     title: "Red Alert",
-    folder: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/pe17f8VDfzbvbHSAKAlcORtBHmW.jpg',
+    folder: 'assets/folders/red_alert.jpg',
     synopsi: "In the world of international crime, INTERPOL issues a Red Alert, which begins a hunt to capture the world's most notorious art thief.",
     year: '2021',
     category: 'Action',
@@ -427,7 +427,7 @@ export const getMovies = () => ([
   {
     id: 26,
     title: "The Predator: The Hunt",
-    folder: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/g7Ii9sYAFG96W7cvMQ4zXq39RJ5.jpg',
+    folder: 'assets/folders/the_predator.jpg',
     synopsi: "Set in the world of the Comanche Nation in the early 1700s, 'The Predator: The Hunt' is the untold story of a highly skilled young warrior desperate to protect her people from impending danger. She pursues and ultimately confronts her prey, which turns out to be a highly evolved alien predator with a technologically advanced arsenal, resulting in a brutal and terrifying showdown between the adversaries.",
     year: '2022',
     category: 'Action',
@@ -444,7 +444,7 @@ export const getMovies = () => ([
   {
     id: 27,
     title: "Spider-Man: No Homecoming",
-    folder: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/fVzXp3NwovUlLe7fvoRynCmBPNc.jpg',
+    folder: 'assets/folders/spider-man.jpg',
     synopsi: "Peter Parker is unmasked and can no longer separate his normal life from the great risks of being a superhero. When he asks Doctor Strange for help, the stakes become even more dangerous, forcing him to discover what it really means to be Spider-Man...",
     year: '2021',
     category: 'Action',
@@ -461,7 +461,7 @@ export const getMovies = () => ([
   {
     id: 28,
     title: "Halo",
-    folder: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/eO0QV5qJaEngP1Ax9w3eV6bJG2f.jpg',
+    folder: 'assets/folders/halo.jpg',
     synopsi: "An epic battle takes place in the 26th century between the human race and an alien species known as the Covenant.",
     year: '2021',
     category: 'Fantasy',
@@ -478,7 +478,7 @@ export const getMovies = () => ([
   {
     id: 29,
     title: "Doctor Strange in the Multiverse of Madness",
-    folder: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/boIgXXUhw5O3oVkhXsE6SJZkmYo.jpg',
+    folder: 'assets/folders/dr_strange.jpg',
     synopsi: "Doctor Strange, with the help of mystical allies old and new, traverses the dangerous and mind-bending alternate realities of the Multiverse to face a mysterious new adversary.",
     year: '2022',
     category: 'Fantasy',
@@ -495,7 +495,7 @@ export const getMovies = () => ([
   {
     id: 30,
     title: "News of a Kidnapping",
-    folder: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/gNlnZcrAKVkL2j7U1CN4EuqpLB9.jpg',
+    folder: 'assets/folders/news.jpg',
     synopsi: "Early 90's, Colombia. The government tries to fight drug trafficking with extradition laws. Maruja Pachón and Beatriz Villamizar, wife and sister of Deputy Alberto Villamizar, are kidnapped by the Extraditables. The police begin a search to find clues that bring them closer to the kidnappers, and Alberto joins the operation to rescue his family.",
     year: '2022',
     category: 'Drama',
@@ -512,7 +512,7 @@ export const getMovies = () => ([
   {
     id: 31,
     title: "The Walking Dead",
-    folder: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/xf9wuDcqlUPWABZNeDKPbZUjWx0.jpg',
+    folder: 'assets/folders/the_walking_dead.jpg',
     synopsi: "In post-apocalyptic America, a small group of survivors continues to travel in search of a new home away from the undead. Desperation for safety and supplies constantly pushes them to the brink of sanity.",
     year: '2010',
     category: 'Drama',
@@ -529,7 +529,7 @@ export const getMovies = () => ([
   {
     id: 32,
     title: "The Simpsons",
-    folder: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/9n4dv0aGRb8Ma15H7jbWsY7Eg0N.jpg',
+    folder: 'assets/folders/the_simpsons.jpg',
     synopsi: "The Simpsons live in the city of Springfield, an American city like any other. Homer works as a safety inspector at a nuclear power plant, Marge tries to keep the peace in her family, Bart is a mischievous 10-year-old boy, 8-year-old Lisa is the smart one in the family who loves to play the saxophone and is a vegetarian and little Maggie conquers everyone while she doesn't let go of her pacifier. And the series has a rich, and often strange, universe of characters that also inhabit Springfield. With clever scripts, subversive humor and playfully witty, the series makes fun of itself and everyone else who gets in its way.",
     year: '1989',
     category: 'Animation',
@@ -546,7 +546,7 @@ export const getMovies = () => ([
   {
     id: 33,
     title: "Minions: The Origin of Gru",
-    folder: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/pllef3lgTNdMEWOwtgIjDvYOeVX.jpg',
+    folder: 'assets/folders/minions.jpg',
     synopsi: "The continuation of the Minions' adventures, always in search of a tyrannical leader. This time, they help a Gru as a child, figuring out how to be a villain.",
     year: '2022',
     category: 'Animation',
@@ -563,7 +563,7 @@ export const getMovies = () => ([
   {
     id: 34,
     title: "The Adam Project",
-    folder: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/11MJy6lkt7yTEIowEPIkaK4B7lM.jpg',
+    folder: 'assets/folders/adam_project.jpg',
     synopsi: "After traveling through time and accidentally ending up in 2022, fighter pilot Adam Reed teams up with his 12-year-old self to save the future.",
     year: '2022',
     category: 'Comedy',
@@ -580,7 +580,7 @@ export const getMovies = () => ([
   {
     id: 35,
     title: "Don't Look Up",
-    folder: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/6Sc7Tjt7aPsdghYK32mDMFeZkqJ.jpg',
+    folder: 'assets/folders/dont_look_up.jpg',
     synopsi: "Two astronomers discover a deadly comet heading towards Earth and set out on a media tour to warn humanity. But nobody seems to care much.",
     year: '2021',
     category: 'Comedy',
