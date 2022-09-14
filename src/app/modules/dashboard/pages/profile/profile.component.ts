7db26a7d34edc4d631f0b80a218699cd35d81ea5
caducasara <carloscasara98@gmail.com>
2022-09-14 16:03:21 -0300

    FEAT: Add user movies more watched in the profile page

--- src/app/modules/dashboard/pages/profile/profile.component.ts
+++ src/app/modules/dashboard/pages/profile/profile.component.ts
@@ -1,8 +1,11 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Movie } from 'src/app/interfaces/Movie';
 import { User } from 'src/app/interfaces/User';
 import { AuthService } from 'src/app/modules/auth/auth.service';
+import { DbService } from 'src/app/service/db.service';
 import { NetflixService } from 'src/app/service/netflix.service';
 
 @Component({
@@ -13,16 +16,31 @@ import { NetflixService } from 'src/app/service/netflix.service';
 export class ProfileComponent implements OnInit {
 
   user: User = {} as User;
+  moviesMoreWatched: Movie[] = [];
+
+  subscriptionMoviesMorewatched: Subscription = new Subscription();
 
   constructor(
     private netflix: NetflixService,
     private auth: AuthService,
     private location: Location,
-    private router: Router
+    private router: Router,
+    private dbService: DbService
   ) { }
 
   ngOnInit(): void {
     this.user =  this.netflix.getUserLogged();
+
+    this.subscriptionMoviesMorewatched.add(
+      this.dbService.getMoviesMoreWatchedByUser(this.user.email as string)
+        .subscribe(movies => {
+          this.moviesMoreWatched = movies;
+        })
+    )
+  }
+
+  ngOnDestroy() {
+    this.subscriptionMoviesMorewatched.unsubscribe();
   }
 
   handleClickLogout(){
--- src/app/service/db.service.ts
+++ src/app/service/db.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { Movie } from '../interfaces/Movie';
+import { User, UserData, UserMoviesWatched } from '../interfaces/User';
+import { sortUserMoreMoviesWatchedArr } from '../utils/sortMoviesArray';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +14,40 @@ export class DbService {
 
   get getMovies(): Observable<Movie[]> {
     return this.http.get<Movie[]>('assets/database/movies.json');
+  }
+
+  getUserById(userId: number): Observable<User> {
+    return this.http.get<User[]>('assets/database/users.json').pipe(
+      map(users => {
+        return users.find(user => user.id === userId) as User;
+      })
+    );
+  }
+
+  getMoviesMoreWatchedByUser(userEmail: string): Observable<Movie[]>{
+    const users = localStorage.getItem('users')
+      ? JSON.parse(localStorage.getItem('users') as string)
+      : [];
+
+      const user = users.find((user: UserData) => user.userEmail === userEmail);
+      user.movies.sort(sortUserMoreMoviesWatchedArr);
+
+      console.log(user.movies)
+
+    return this.getMovies.pipe(
+      map(movies => {
+        let moviesWatched: Movie[] = [];
+
+        user.movies.forEach((userMovieWatched: UserMoviesWatched) => {
+          movies.forEach(movie => {
+            if(userMovieWatched.movieId === movie.id){
+              moviesWatched.push(movie)
+            }
+          })
+        })
+
+        return moviesWatched;
+      })
+    );
   }
 }
