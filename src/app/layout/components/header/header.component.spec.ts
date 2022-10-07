bacdbd7ff603c24012cf892ed9fbc5a008788589
caducasara <carloscasara98@gmail.com>
2022-10-07 16:00:35 -0300

    test: finishing unit tests on components

--- src/app/layout/components/header/header.component.spec.ts
+++ src/app/layout/components/header/header.component.spec.ts
@@ -2,8 +2,11 @@ import { HttpClient } from '@angular/common/http';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatMenuModule } from '@angular/material/menu';
+import { of } from 'rxjs';
 import { Countries } from 'src/app/enums/countries';
+import { User } from 'src/app/interfaces/User';
 import { NetflixService } from 'src/app/service/netflix.service';
+import { getUserMock } from 'src/app/tests/mocks/Users';
 
 import { HeaderComponent } from './header.component';
 
@@ -11,7 +14,8 @@ describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
   let netflixService: NetflixService;
-  let http: HttpClient;
+
+  const userLoggedMock = getUserMock();
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -30,7 +34,6 @@ describe('HeaderComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(HeaderComponent);
     netflixService = TestBed.inject(NetflixService);
-    http = TestBed.inject(HttpClient);
     component = fixture.componentInstance;
 
     fixture.detectChanges();
@@ -38,5 +41,15 @@ describe('HeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('(U) HeaderComponent -> Should user profile picture to equal a user logged', () => {
+    const spyGetUserLogged = spyOnProperty(netflixService, 'getUserLogged', 'get')
+     .and.returnValue(of(userLoggedMock as User));
+
+     component.ngOnInit();
+     fixture.detectChanges();
+
+     expect(component.userProfilePicture).toEqual(userLoggedMock.picture);
   });
 });
--- src/app/modules/auth/signin/signin.component.spec.ts
+++ src/app/modules/auth/signin/signin.component.spec.ts
@@ -1,25 +1,55 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 import { SiginComponent } from './signin.component';
 
 describe('SiginComponent', () => {
   let component: SiginComponent;
   let fixture: ComponentFixture<SiginComponent>;
+  let authService: AuthService;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SiginComponent ],
+      declarations: [SiginComponent],
       providers: [FormBuilder]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(SiginComponent);
     component = fixture.componentInstance;
+    authService = TestBed.inject(AuthService);
+    router = TestBed.inject(Router);
 
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('(U) signInSubmit -> Should not authenticate user', () => {
+    component.createSignInForm();
+    component.signInSubmit();
+
+    expect(component.isUserNotAuthenticated).toBeTrue();
+  });
+
+  it('(U) signInSubmit -> Should authenticate user', () => {
+    const spySignIn = spyOn(authService, 'signIn');
+    const spyGetUserAuthenticated = spyOn(authService, 'getUserAuthenticated')
+      .and.returnValue(true);
+    const spyRouter = spyOn(router, 'navigate');
+
+    component.createSignInForm();
+    component.signInForm.controls['email'].setValue('user1@teste.com');
+    component.signInForm.controls['password'].setValue('teste1');
+
+    component.signInSubmit();
+
+    expect(spySignIn).toHaveBeenCalled();
+    expect(component.isUserNotAuthenticated).toEqual(!spyGetUserAuthenticated);
+    expect(spyRouter).toHaveBeenCalledWith(['/']);
   });
 });
